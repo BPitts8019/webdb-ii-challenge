edab3e1f8c9b53ddd7a08b8b720ecac5173f1560
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-07 18:43:08 -0700

    Added 404 and 500 endpoints

--- server.js
+++ server.js
@@ -10,4 +10,19 @@ server.get("/", (req, res, next) => {
 
 server.use("/api/cars", carsRouter);
 
+//404 not found
+server.use((req, res) => {
+   res.status(404).json({
+      message: "Route was not found"
+   });
+});
+
+//Catch 500 errors
+server.use((error, req, res, next) => {
+   console.log(error.toString());
+   res.status(500).json({
+      data: error.toString()
+   });
+});
+
 module.exports = server;
