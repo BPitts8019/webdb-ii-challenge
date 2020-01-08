3b6ebc420d437cad9c03ac66003aa891d350e09a
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-07 19:15:18 -0700

    Updated vin schema to be a string 17 characters in length

--- data/migrations/20191220183316_create-cars.js
+++ data/migrations/20191220183316_create-cars.js
@@ -3,7 +3,7 @@ const CARS = "cars";
 exports.up = async function (knex) {
    await knex.schema.createTable(CARS, table => {
       table.increments();
-      table.integer("VIN").notNullable().unique();
+      table.string("VIN", 17).notNullable().unique();
       table.text("make").notNullable();
       table.text("model").notNullable();
       table.integer("mileage").notNullable();
